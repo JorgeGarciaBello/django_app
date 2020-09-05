878ebd8d4b851d88f527c7ae43418cfcd6029ea7
JorgeGarciaBello <jogabell@hotmail.com>
2020-09-05 18:32:45 -0500

    Se inicia modelos de categorias y series de tutoriales

--- main/models.py
+++ main/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from datetime import datetime
 from django.utils import timezone
 
+class TutorialCategory(models.Model):
+	tutorial_category = models.CharField(max_length=200)
+	category_summary = models.CharField(max_length=200)
+	category_slug = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = "Categories"
+
+	def _str_(self):
+		return self.tutorial_category
+
+class TutorialSeries(models.Model):
+	tutorial_series = models.CharField(max_length=200)
+	tutorial_description = models.CharField(max_length=200)
+	tutorial_type = models.CharField(max_length=200)
+
+
+
+
 # Create your models here.
 class Tutorial(models.Model):
 	tutorial_title = models.CharField(max_length=200)
